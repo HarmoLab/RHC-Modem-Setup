1db03043a44e21d3a414deadcbd87ab30b79e4ae
nagata <nagata@complex.ist.hokudai.ac.jp>
2018-12-13 19:41:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RHC Modem Setup Tool
+
+
+### Usage
+1. モデムをPCに挿す
+2. $ ./RHC-Modem-Setup.sh
+3. 再起動するのをしばらく待つ
+4. 適当なWebサイトを開き接続を確認する
---
+++ RHC-Modem-Setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" -d 'Page=SetProfileInfo&index=1&profile_name=CID1&total=0&ip_type=0&auth=3&username=sora&password=sora&apn=soracom.io&Result=0&profile_list=0' http://am.webcm/cgi-bin/qcmap_web_cgi
